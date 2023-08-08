5a2ae966ff55cc05fc81a1853047a3e5f1650dfc
kawillrich <66137285+kawillrich@users.noreply.github.com>
2023-08-08 12:59:32 -0400

    updates

--- Overworld.js
+++ Overworld.js
@@ -7,25 +7,43 @@ class Overworld {
 
     init() {
         console.log('hi from overworld', this);
+        
         const image = new Image();
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0)
         };
         image.src = "/images/maps/tileset-outdoor-gameworld-lower.png";
 
-        const x = 0;
-        const y = 0;
+        const x = 5;
+        const y = 10;
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,
+                0,
+                0,
+                32,
+                48,
+                x * 20,
+                y * 16 -12,
+                32,
+                48
+
+                 )
+        }
+        shadow.src="images/characters/shadow.png";
 
         const hero = new Image();
         hero.onload = () => {
-            this.ctx.drawImage( //image, top, left, width of cut, height of cut, 
+            this.ctx.drawImage( //image, start crop x, start crop y, width of cut, height of cut, x placement, y placement
                 hero, 
                 0, 
                 0,
                 32,
                 48,
-                x,
-                y,
+                x * 20,
+                y * 16 - 12,
                 32,
                 48
                 )
