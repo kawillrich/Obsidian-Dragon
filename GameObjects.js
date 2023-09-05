1819fc5eed0437d6fa8188dcb0001213caa009a0
kawillrich <66137285+kawillrich@users.noreply.github.com>
2023-09-05 16:40:35 -0400

    fix: remove vx, vy

--- GameObjects.js
+++ GameObjects.js
@@ -3,8 +3,6 @@ class GameObject {
         this.isMounted = false;
         this.x = config.x || 0;
         this.y = config.y || 0;
-        this.vx = config.vx || 0;
-        this.vy = config.vy || 0;
         this.direction = config.direction || "down";
         this.sprite = new Sprite({
             gameObject: this,
